3349a275e6a16b5ebc04b6edea228d78509e380d
Neek Sandhu <neek.sandhu@outlook.com>
2017-11-20 00:08:54 -0800

    Fix

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,6 +2,6 @@ export declare function setPathSeparator(separator: string): void;
 export declare function join(...paths: string[]): string;
 export declare function basename(path: string): string;
 export declare function extname(path: string): string;
-export declare function toWinPath(path: string): any;
-export declare function toUnixPath(path: string): any;
+export declare function toWinPath(path: string): string;
+export declare function toUnixPath(path: string): string;
 export declare function dirname(path: string): string;
