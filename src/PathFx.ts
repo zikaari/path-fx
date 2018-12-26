e8d74450531b347d19c6fbc2cb1cbdc36dbb4423
Neek Sandhu <neek.sandhu@outlook.com>
2018-12-26 12:57:44 -0800

    fix: accomodate for backslashes in unix filenames
    Library consumers can now access the `PathFx` class export that allows
    isolated path utils for win32 and unix like paths. Previously setting
    the platform was a global operation.
    
    BREAKING CHANGE:
    
    Remove `setPathSeparator` (at least for now)

---
+++ src/PathFx.ts
@@ -0,0 +1,140 @@
+import * as _isRelative from 'is-relative'
+
+const CONSTANTS = {
+    ANY_LEADING_SEP_RE: /^[\\\/]+/,
+    ANY_TRAILING_SEP_RE: /[\\\/]+$/,
+    UNIX_JOIN_CHARACTER: '/',
+    UNIX_SEP_MATCH_RE: /[\/]+/g,
+    UNIX_SEP_NEGATE_RE: /[^\/]+/g,
+    WIN_JOIN_CHARACTER: '\\',
+    WIN_SEP_MATCH_RE: /[\/\\]+/g,
+    WIN_SEP_NEGATE_RE: /[^\/\\]+/g,
+}
+
+export class PathFx {
+    constructor(private platform: 'win32' | 'unix') { }
+
+    public join = (...paths: string[]) => {
+        const composed = []
+        const hasLeadingSep = CONSTANTS.ANY_LEADING_SEP_RE.test(paths[0])
+        const hasTrailingSep = CONSTANTS.ANY_TRAILING_SEP_RE.test(paths[paths.length - 1])
+        for (let i = 0; i < paths.length; i++) {
+            const path = paths[i]
+            const parts = this.splitPath(path)
+            for (let j = 0; j < parts.length; j++) {
+                const part = parts[j]
+                if (part === '.') {
+                    continue
+                } else if (/^[.]{2,}$/.test(part)) {
+                    composed.pop()
+                } else {
+                    composed.push(part)
+                }
+            }
+        }
+        if (hasLeadingSep) {
+            composed.unshift('')
+        }
+        if (hasTrailingSep) {
+            composed.push('')
+        }
+        return composed.join(this.platform === 'win32' ? CONSTANTS.WIN_JOIN_CHARACTER : CONSTANTS.UNIX_JOIN_CHARACTER)
+    }
+
+    /**
+     * Relative path `to` something `from` something
+     * `from` and `to` MUST BE absolute paths
+     *
+     * @param from Absolute from
+     * @param to Absolute to
+     */
+    public relative = (from: string, to: string) => {
+        if (this.isRelative(from) || this.isRelative(to)) {
+            throw new Error(`'from' and 'to' both must be absolute paths`)
+        }
+        const fromFrags = this.splitPath(from)
+        const toFrags = this.splitPath(to)
+        const hasTrailingSep = CONSTANTS.ANY_TRAILING_SEP_RE.test(to)
+        const joinCharacter = this.platform === 'win32' ? CONSTANTS.WIN_JOIN_CHARACTER : CONSTANTS.UNIX_JOIN_CHARACTER
+        for (let i = 0; i < fromFrags.length; i++) {
+            const fromFrag = fromFrags[i]
+            if (fromFrag !== toFrags[i]) {
+                const remainder = fromFrags.length - i
+                return Array(remainder)
+                    .fill('..')
+                    .concat(toFrags.slice(i))
+                    .join(joinCharacter) + (hasTrailingSep ? joinCharacter : '')
+            }
+        }
+        return toFrags.slice(fromFrags.length).join(joinCharacter) + (hasTrailingSep ? joinCharacter : '')
+    }
+
+    public isPathInside = (containingPath: string, path: string): boolean => {
+        const pathFrags = this.splitPath(path)
+        const contPathFrags = this.splitPath(containingPath)
+        return pathFrags.every((fragment, idx) => fragment === contPathFrags[idx])
+    }
+
+    public isRelative = (path: string): boolean => {
+        return _isRelative(path)
+    }
+
+    public pathDepth = (path: string) => {
+        const frags = this.splitPath(path)
+        return frags ? frags.length : 0
+    }
+
+    public basename = (path: string): string => {
+        const frags = this.splitPath(path)
+        return frags ? frags[frags.length - 1] : ''
+    }
+
+    public normalize = (path: string): string => {
+        return this.join(path)
+    }
+
+    public extname = (path: string): string => {
+        const name = this.basename(path)
+        const idx = name.lastIndexOf('.')
+        return idx < 0 ? '' : name.slice(idx)
+    }
+
+    public toWinPath = (path: string) => {
+        this.checkPath(path)
+        return path.replace(CONSTANTS.UNIX_SEP_MATCH_RE, CONSTANTS.WIN_JOIN_CHARACTER)
+    }
+
+    public toUnixPath = (path: string) => {
+        this.checkPath(path)
+        return path.replace(CONSTANTS.WIN_SEP_MATCH_RE, CONSTANTS.UNIX_JOIN_CHARACTER)
+    }
+
+    public dirname = (path: string) => {
+        const parts = this.splitPath(path)
+        const hasLeadingSep = CONSTANTS.ANY_LEADING_SEP_RE.test(path)
+        parts.pop()
+        if (parts.length === 0) {
+            return '.'
+        }
+        if (hasLeadingSep) {
+            parts.unshift('')
+        }
+        return parts.join(this.platform === 'win32' ? CONSTANTS.WIN_JOIN_CHARACTER : CONSTANTS.UNIX_JOIN_CHARACTER)
+    }
+
+    public splitPath = (path: string): string[] => {
+        this.checkPath(path)
+        return path.match(this.platform === 'win32' ? CONSTANTS.WIN_SEP_NEGATE_RE : CONSTANTS.UNIX_SEP_NEGATE_RE) || []
+    }
+
+    public removeTrailingSlashes = (path: string) => {
+        this.checkPath(path)
+        return path.replace(CONSTANTS.ANY_TRAILING_SEP_RE, '')
+    }
+
+    private checkPath = (path: string) => {
+        if (typeof path !== 'string') {
+            throw new TypeError(`Path must be a string. Received ${typeof path}`)
+        }
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,127 +1,36 @@
-import * as _isRelative from 'is-relative';
+import { PathFx } from './PathFx'
 
-const platform = typeof navigator === 'object' ?
-    navigator.platform :
-    typeof process === 'object' ?
-        process.platform :
-        '';
-let pathSeparator = /win/i.test(platform || '') ? '\\' : '/';
+const platform = /win32/i.test((typeof navigator === 'object' && navigator.platform) || (typeof process === 'object' && process.platform)) ? 'win32' : 'unix'
 
-export function setPathSeparator(separator: string) {
-    pathSeparator = separator;
-}
+const {
+    join,
+    relative,
+    isRelative,
+    isPathInside,
+    pathDepth,
+    basename,
+    extname,
+    dirname,
+    normalize,
+    toWinPath,
+    toUnixPath,
+    splitPath,
+    removeTrailingSlashes,
+} = new PathFx(platform)
 
-export function join(...paths: string[]) {
-    const composed = [];
-    const hasLeadingSep = /^[\\\/]/.test(paths[0]);
-    const hasTrailingSep = /[\\\/]$/.test(paths[paths.length - 1]);
-    for (let i = 0; i < paths.length; i++) {
-        const path = paths[i];
-        checkPath(path);
-        const parts = path.match(/[^\/\\]+/g);
-        for (let j = 0; j < parts.length; j++) {
-            const part = parts[j];
-            if (part === '.') {
-                continue;
-            } else if (/^[.]{2,}$/.test(part)) {
-                composed.pop();
-            } else {
-                composed.push(part);
-            }
-        }
-    }
-    return (hasLeadingSep ? pathSeparator : '') + composed.join(pathSeparator) + (hasTrailingSep ? pathSeparator : '');
-}
-
-export function basename(path: string): string {
-    checkPath(path);
-    return (removeTrailingSlashes(path || '').match(/[^\\\/]+$/) || [])[0] || '';
-}
-
-export function normalize(path: string): string {
-    return join(path);
-}
-
-export function extname(path: string): string {
-    const name = basename(path);
-    const idx = name.lastIndexOf('.');
-    return idx < 0 ? '' : name.slice(idx);
-}
-
-export function toWinPath(path: string) {
-    checkPath(path);
-    return path.replace(/[\/\\]+/g, '\\');
-}
-
-export function toUnixPath(path: string) {
-    checkPath(path);
-    return path.replace(/[\/\\]+/g, '/');
-}
-
-export function dirname(path: string) {
-    checkPath(path);
-    const leadingSep = path.search(/[\/\\]/) === 0 ? pathSeparator : '';
-    const parts = path.match(/[^\\\/]+/g) || [];
-    parts.pop();
-    const dir = parts.join(pathSeparator);
-    return dir.length === 0 ? '.' : leadingSep + dir;
-}
-
-export function isPathInside(containingPath: string, path: string): boolean {
-    const pathFrags = splitPath(path);
-    const contPathFrags = splitPath(containingPath);
-    return pathFrags.every((fragment, idx) => fragment === contPathFrags[idx]);
-}
-
-export function isRelative(path: string): boolean {
-    return _isRelative(path);
-}
-
-export function pathDepth(path: string) {
-    return splitPath(path).length;
-}
-
-function checkPath(path: string) {
-    if (typeof path !== 'string') {
-        throw new TypeError(`Path must be a string. Received ${typeof path}`);
-    }
-}
-
-export function removeTrailingSlashes(path: string) {
-    if (typeof path === 'string' && path.length > 0) {
-        return path.replace(/[\/\\]+$/, '');
-    }
-}
-
-export function splitPath(path: string): string[] {
-    checkPath(path);
-    const frags = removeTrailingSlashes(path).split(/[\/\\]+/g);
-    if (frags[0] === '') { // empty caused by leading sep
-        frags.shift();
-    }
-    return frags;
-}
-
-/**
- * Relative path `to` something `from` something
- * `from` and `to` MUST BE absolute paths
- *
- * @param from Absolute from
- * @param to Absolute to
- */
-export function relative(from: string, to: string) {
-    if (isRelative(from) || isRelative(to)) {
-        throw new Error(`'from' and 'to' both must be absolute paths`);
-    }
-    const fromFrags = splitPath(from);
-    const toFrags = splitPath(to);
-    const hasTrailingSep = /[\\\/]$/.test(to);
-    for (let i = 0; i < fromFrags.length; i++) {
-        const fromFrag = fromFrags[i];
-        if (fromFrag !== toFrags[i]) {
-            const remainder = fromFrags.length - i;
-            return Array(remainder).fill('..').concat(toFrags.slice(i)).join(pathSeparator) + (hasTrailingSep ? pathSeparator : '');
-        }
-    }
-    return toFrags.slice(fromFrags.length).join(pathSeparator) + (hasTrailingSep ? pathSeparator : '');
+export {
+    PathFx,
+    join,
+    relative,
+    isRelative,
+    isPathInside,
+    pathDepth,
+    basename,
+    extname,
+    dirname,
+    normalize,
+    toWinPath,
+    toUnixPath,
+    splitPath,
+    removeTrailingSlashes,
 }
